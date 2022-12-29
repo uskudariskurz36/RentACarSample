d03816186506db265bf2f758d11ef0fe7245e88d
Murat Başeren <uskudar.iskur.z36@gmail.com>
2022-12-29 08:58:51 +0300

    Add project files.

---
+++ RentACarSample/Entities/Member.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACarSample.Entities
+{
+    public class Member
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Password { get; set; }
+    }
+}
---
+++ RentACarSample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RentACarSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
