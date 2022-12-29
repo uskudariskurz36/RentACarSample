1e28e251b549322b111183f1300768b46e9250e3
Murat Başeren <uskudar.iskur.z36@gmail.com>
2022-12-29 09:48:15 +0300

    .

--- RentACarSample/Controllers/AccountController.cs
+++ RentACarSample/Controllers/AccountController.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RentACarSample.Models;
 
 namespace RentACarSample.Controllers
 {
     public class AccountController : Controller
     {
         public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
         {
             return View();
         }
---
+++ RentACarSample/Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACarSample.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [StringLength(30)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Password { get; set; }
+    }
+}
