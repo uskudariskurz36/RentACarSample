0798a759ce72e74fa55b25dd831c47311e4f850c
Murat Başeren <uskudar.iskur.z36@gmail.com>
2022-12-29 09:39:30 +0300

    .

---
+++ RentACarSample/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACarSample.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
