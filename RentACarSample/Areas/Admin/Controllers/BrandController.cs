95e10374d42d5615432b91225d410ef6d636f85c
Murat Başeren <uskudar.iskur.z36@gmail.com>
2023-01-02 13:16:52 +0300

    .

--- RentACarSample/Areas/Admin/Controllers/BrandController.cs
+++ RentACarSample/Areas/Admin/Controllers/BrandController.cs
@@ -33,15 +33,72 @@ namespace RentACarSample.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-                Brand brand = new Brand();
-                brand.Name = model.Name;
-                brand.Description = model.Description;
-                brand.Hidden = model.Hidden;
+                if (_databaseContext.Brands.Any(x => x.Name.ToLower() == model.Name.ToLower()))
+                {
+                    ModelState.AddModelError(nameof(model.Name), "İlgili marka zaten bulunmaktadır.");
+                }
+                else
+                {
+                    Brand brand = new Brand();
+                    brand.Name = model.Name;
+                    brand.Description = model.Description;
+                    brand.Hidden = model.Hidden;
 
-                _databaseContext.Brands.Add(brand);
-                _databaseContext.SaveChanges();
+                    _databaseContext.Brands.Add(brand);
+                    _databaseContext.SaveChanges();
 
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Brand brand = _databaseContext.Brands.Find(id);
+
+            if (brand == null)
+            {
                 return RedirectToAction(nameof(Index));
+            }
+
+            BrandEditViewModel model = new BrandEditViewModel();
+            model.Name = brand.Name;
+            model.Description = brand.Description;
+            model.Hidden = brand.Hidden;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, BrandEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Brand brand = _databaseContext.Brands.Find(id);
+
+                if (brand == null)
+                {
+                    ModelState.AddModelError("", "İlgili marka bulunamadı.");
+                }
+                else
+                {
+                    if (_databaseContext.Brands.Any(x => x.Name.ToLower() == model.Name.ToLower() && x.Id != id))
+                    {
+                        ModelState.AddModelError(nameof(model.Name), "İlgili marka zaten bulunmaktadır.");
+                    }
+                    else
+                    {
+                        brand.Name = model.Name;
+                        brand.Description = model.Description;
+                        brand.Hidden = model.Hidden;
+
+                        _databaseContext.SaveChanges();
+
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
             }
 
             return View(model);
---
+++ RentACarSample/Areas/Admin/Models/BrandEditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACarSample.Areas.Admin.Models
+{
+    public class BrandEditViewModel
+    {
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [StringLength(250)]
+        public string? Description { get; set; }
+
+        public bool Hidden { get; set; }
+    }
+}
