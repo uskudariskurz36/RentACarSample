53972804996ed74468d6b50458256dbb4a7e9892
Murat Başeren <uskudar.iskur.z36@gmail.com>
2023-01-05 13:11:57 +0300

    .

--- RentACarSample/Areas/Admin/Controllers/RentController.cs
+++ RentACarSample/Areas/Admin/Controllers/RentController.cs
@@ -21,6 +21,43 @@ namespace RentACarSample.Areas.Admin.Controllers
             return View();
         }
 
+        // GET : /Admin/Rent/GetRentReceivePartial?rentId=guid
+        public IActionResult GetRentReceivePartial(Guid rentId)
+        {
+            Rent rent = _databaseContext.Rents.Include(x => x.Car).Include(x => x.Customer).FirstOrDefault(x => x.Id == rentId);
+
+            return PartialView("_RentReceivePartial", rent);
+        }
+
+        // POST : /Admin/Rent/Receive/guid
+        [HttpPost]
+        public IActionResult Receive(Guid id)
+        {
+            Rent rent = _databaseContext.Rents.Find(id);
+
+            CloseModalPartialViewModel partialModel = new CloseModalPartialViewModel();
+            partialModel.ModalId = "modalReceive";
+            partialModel.ShowToastr = true;
+
+            if (rent != null)
+            {
+                rent.Received = true;
+                _databaseContext.SaveChanges();
+
+                partialModel.ToastrType = "success";
+                partialModel.ToastrTitle = "Araç Teslim";
+                partialModel.ToastrMessage = "Araç teslim alınmıştır.";
+            }
+            else
+            {
+                partialModel.ToastrType = "warning";
+                partialModel.ToastrTitle = "Uyarı";
+                partialModel.ToastrMessage = "Kiralama verisi bulunamadı.";
+            }
+
+            return PartialView("_CloseModalPartial", partialModel);
+        }
+
         // GET : /Admin/Rent/GetRentListPartial
         public IActionResult GetRentListPartial(string filter = "none")
         {
@@ -30,7 +67,7 @@ namespace RentACarSample.Areas.Admin.Controllers
             {
                 rents = _databaseContext.Rents.Where(x => x.Received).Include(x => x.Car).Include(x => x.Customer).ToList();
             }
-            else if(filter == "nonreceived")
+            else if (filter == "nonreceived")
             {
                 rents = _databaseContext.Rents.Where(x => !x.Received).Include(x => x.Car).Include(x => x.Customer).ToList();
             }
@@ -47,7 +84,7 @@ namespace RentACarSample.Areas.Admin.Controllers
         {
             RentCreateViewModel model = new RentCreateViewModel();
             model.ReceivedDate = DateTime.Now.Date;
-            
+
             // Şu an kiralanmamı ve is available=true olan araçlar seçilebilmeli.
             List<Car> cars = _databaseContext.Cars
                 .Where(car => !car.Rents.Any(rent => rent.CarId == car.Id && !rent.Received) && car.IsAvailable).ToList();
@@ -66,39 +103,56 @@ namespace RentACarSample.Areas.Admin.Controllers
             return Json(car.DailyPrice);
         }
 
+        // GET : /Admin/Rent/GetDescData?rentId=1
+        public IActionResult GetDescData(Guid rentId)
+        {
+            return Json(_databaseContext.Rents.Find(rentId).Description);
+        }
+
         [HttpPost]
         public IActionResult Create(RentCreateViewModel model)
         {
             if (ModelState.IsValid)
             {
-                Rent rent = new Rent
+                if (model.ReceivedDate < DateTime.Now)
                 {
-                    CarId = model.CarId,
-                    CustomerId = model.CustomerId,
-                    Price = model.Price,
-                    KM = model.KM,
-                    ReceivedDate = model.ReceivedDate,
-                    Description = model.Description,
-                    Received = false,
-                    RentDate = DateTime.Now
-                };
-
-                _databaseContext.Rents.Add(rent);
-                _databaseContext.SaveChanges();
-
-                CloseModalPartialViewModel partialModel = new CloseModalPartialViewModel
+                    ModelState.AddModelError(nameof(model.ReceivedDate), $"Teslim tarihi {DateTime.Now.ToString("g")} tarihinden eski olamaz!");
+                }
+                else
                 {
-                    ModalId = "modalCreate",
-                    ShowToastr = true,
-                    ToastrType = "success",
-                    ToastrTitle = "Kiralama Yapıldı",
-                    ToastrMessage = "Araç kiralama kaydı başarıyla yapılmıştır."
-                };
+                    Rent rent = new Rent
+                    {
+                        CarId = model.CarId,
+                        CustomerId = model.CustomerId,
+                        Price = model.Price,
+                        KM = model.KM,
+                        ReceivedDate = model.ReceivedDate,
+                        Description = model.Description,
+                        Received = false,
+                        RentDate = DateTime.Now
+                    };
 
-                return PartialView("_CloseModalPartial", partialModel);
+                    _databaseContext.Rents.Add(rent);
+                    _databaseContext.SaveChanges();
+
+                    CloseModalPartialViewModel partialModel = new CloseModalPartialViewModel
+                    {
+                        ModalId = "modalCreate",
+                        ShowToastr = true,
+                        ToastrType = "success",
+                        ToastrTitle = "Kiralama Yapıldı",
+                        ToastrMessage = "Araç kiralama kaydı başarıyla yapılmıştır."
+                    };
+
+                    return PartialView("_CloseModalPartial", partialModel);
+                }
             }
 
-            model.Cars = new SelectList(_databaseContext.Cars.ToList(), nameof(Car.Id), nameof(Car.Plate));
+            // Şu an kiralanmamı ve is available=true olan araçlar seçilebilmeli.
+            List<Car> cars = _databaseContext.Cars
+                .Where(car => !car.Rents.Any(rent => rent.CarId == car.Id && !rent.Received) && car.IsAvailable).ToList();
+
+            model.Cars = new SelectList(cars, nameof(Car.Id), nameof(Car.Plate));
             model.Customers = new SelectList(_databaseContext.Customers.ToList(), nameof(Customer.Id), nameof(Customer.FullName));
 
             return PartialView("_CreateRentPartial", model);
